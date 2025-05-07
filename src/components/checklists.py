d3e92cdce007d949601681d9e95f28457bab75b9
Mel Chafart <mchaf@carbonleadershipforum.org>
2025-05-06 22:58:12 -0700

    feat: add checklists

---
+++ src/components/checklists.py
@@ -0,0 +1,34 @@
+from dash import html
+import dash_bootstrap_components as dbc
+
+
+def create_checklist(label: str,
+                     checklist: list,
+                     first_item: str,
+                     dropdown_id: str) -> html.Div:
+    """_summary_
+
+    Args:
+        labe (str): _description_
+        dropdown_list (list): _description_
+        first_item (str): _description_
+        id (str): _description_
+
+    Returns:
+        html.Div: _description_
+    """
+
+    checklist = html.Div(
+        [
+            dbc.Label(label),
+            dbc.Checklist(
+                options=checklist,
+                value=first_item,
+                id=dropdown_id,
+                persistence=True,
+                inputCheckedClassName="border border-primary bg-primary"
+            ),
+        ],
+        className='mb-4'
+    )
+    return checklist
